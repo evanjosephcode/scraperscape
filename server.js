34a0260bb02254f827f40255501997d5ac4ac7cc
evanjosephcode <evanseaman89@gmail.com>
2018-06-09 17:49:41 -0500

    pseudocoded hb and added /:id route

--- server.js
+++ server.js
@@ -93,8 +93,8 @@ app.get("/scrape", function (req, res) {
 
 
 app.get("/", (req, res) => {
-  // res.render("index");
-  res.send("hello world");
+  res.render("index");
+  // res.send("hello world");
 });
 
 app.get("/api/articles", function (req, res) {
@@ -109,3 +109,18 @@ app.get("/api/articles", function (req, res) {
           res.json(err);
       });
 });
+
+app.get("/api/articles/:id", function (req, res) {
+        // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+        db.Article.findOne({ _id: req.params.id })
+            // ..and populate all of the notes associated with it
+            .populate("notes")
+            .then(function (dbArticle) {
+                // If we were able to successfully find an Article with the given id, send it back to the client
+                res.json(dbArticle);
+            })
+            .catch(function (err) {
+                // If an error occurred, send it to the client
+                res.json(err);
+            });
+    });
