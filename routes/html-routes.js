dfd7a5a660bc2acf469ef1547990bca03185163d
evanjosephcode <evanseaman89@gmail.com>
2018-06-11 21:05:09 -0500

    fixed res.render for db objects to show on index

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,8 +3,15 @@ var db = require("./../models");
 module.exports = function (app) {
 
     app.get("/", (req, res) => {
-        // res.render("index");
-        res.send("hello world");
+        db.Article.find({})
+            .then(function (articles) {
+                // If we were able to successfully find Article, send them back to the client
+                res.render('index', { results: articles });
+            })
+            .catch(function (err) {
+                // If an error occurred, send it to the client
+                res.json(err);
+            });
     });
 
 }
